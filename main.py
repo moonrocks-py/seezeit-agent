54aa44274c1d452277d1d82b5a723a2adaee5179
Anton <anton.groezinger@gmail.com>
2025-10-08 08:42:14 +0200

    gitignore/env

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from seezeit-agent!")
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph, END, START
+import requests as re
+from pydantic import BaseModel
+from typing import TypedDict, List
+
+llm = ChatOpenAI(
+    model="gpt-4",
+    temperature=0.9
+)
+
+class AgentState(TypedDict):
+    raw_offer: List[str]
 
 
-if __name__ == "__main__":
-    main()
+def scrape_content(url):
+    response = re.get(
+        url=url
+    )
+    response.raise_for_status()
+    txt = response.text
+    print(txt)
+
+scrape_content("https://www.das-seeglueck.de/")
